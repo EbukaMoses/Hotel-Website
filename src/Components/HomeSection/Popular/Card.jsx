6a14ff95935d8765e6bf655b3a8336a3dd394d0d
Ebuka Moses <okoyemoses83@gmail.com>
2024-03-26 15:05:39 +0100

    added slick slider

--- src/Components/HomeSection/Popular/Card.jsx
+++ src/Components/HomeSection/Popular/Card.jsx
@@ -1,33 +1,103 @@
 import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import PopularData from "./PopData";
+import Slider from "react-slick";
 
+
+
+const SamleNextArrow = (props) => {
+  const { onClick } = props
+  return(
+    <div className="control-btn" onClick={onClick}>
+      <button className="next">
+        <i className="fa fa-long-arrow-alt-right"></i>
+      </button>
+    </div>
+  )
+}
+const SamlePrevArrow = (props) => {
+  const { onClick } = props
+  return(
+    <div className="control-btn" onClick={onClick}>
+      <button className="prev">
+        <i className="fa fa-long-arrow-alt-left"></i>
+      </button>
+    </div>
+  );
+};
 const Card = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    // initialSlide: 0,
+    nextArrow: <SamleNextArrow />,
+    prevArrow: <SamlePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+          // infinite: true,
+          // dots: true,
+        },
+      },
+      // {
+      //   breakpoint: 600,
+      //   settings: {
+      //     slidesToShow: 2,
+      //     slidesToScroll: 2,
+      //     initialSlide: 2,
+      //   },
+      // },
+      // {
+      //   breakpoint: 480,
+      //   settings: {
+      //     slidesToShow: 1,
+      //     slidesToScroll: 1,
+      //   },
+      // },
+    ],
+  };
   return (
     <>
-      <div className="cards">
-        <div className="item">
-          <div className="image">
-            <img src="" alt="" />
-            <i className="fas fa-map-market-alt"></i>
-            <label></label>
-          </div>
+      <Slider {...settings}>
+        {PopularData.map((value) => {
+          return (
+            <div className="cards">
+              <div className="item">
+                <div className="imgae">
+                  <img src={value.image} alt="" />
+                  <i className="fas fa-map-marker-alt">
+                  <label>{value.country}</label></i>
+                </div>
 
-          <div className="rate">
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-          </div>
-          <div className="details">
-            <h2></h2>
-            <div className="boarder"></div>
-            <h3></h3>
-          </div>
-        </div>
-      </div>
+                <div className="rate">
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                </div>
+                <div className="details">
+                  <h2>{value.name}</h2>
+                  <div className="boarder"></div>
+                  <h3>
+                    {value.price}
+                    <span>Per Night</span>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </Slider>
     </>
   );
 };
